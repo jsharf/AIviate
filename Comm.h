dbb4c30af015cf7f43f7db23c87ece68265931d8
Jacob Sharf <jacobsharf@gmail.com>
2014-03-25 14:46:08 -0700

    Fixed communications library Comm.h and Comm.cpp

--- Comm.h
+++ Comm.h
@@ -1,14 +1,27 @@
+#ifndef COMM_H
+#define COMM_H
+
+#include <iostream>
+#include <unistd.h>
 #include <string>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string>
+#include <cstring>
 using namespace std;
 
-const int PACKETSIZE = 2048;
+extern const int PACKETSIZE;
 
 class UDPListener
 {
     public:
-        UDPListener(string port, int debug);
+        UDPListener(string port, int debug=false);
         string listen() const;
     private:
+        struct sockaddr_in remaddr;
         int in_sockfd;
         bool mDebug;
 };
@@ -16,10 +29,12 @@ class UDPListener
 class UDPSender
 {
     public:
-        UDPSender(string port, string url, int debug);
+        UDPSender(string url, string port, int debug=false);
         int send(string msg) const;
     private:
+        sockaddr_in out_addr;
         int out_sockfd;
         bool mDebug;
 };
 
+#endif
