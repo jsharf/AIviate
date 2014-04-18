83c70bfc08463314fda6119e4a758a62f509322c
Jacob Sharf <jacobsharf@gmail.com>
2014-04-17 20:21:53 -0700

    Fixed filtering

--- Comm/Sensor.cpp
+++ Comm/Sensor.cpp
@@ -8,12 +8,12 @@
 // Helps debugging with sensor structs
 std::ostream& operator<<(std::ostream &out, sensorf &rhs)
 {
-    out << "SENSORF: \n{\n\t ax: " \
-    << rhs.ax << "\n\t ay: " << rhs.ay << "\n\t az: " << rhs.az \
-    << "\n\t gx: " << rhs.gx << "\n\t gy: " << rhs.gy << "\n\t gz: " << rhs.gz \
-    << "\n\t mx: " << rhs.mx << "\n\t my: " << rhs.my << "\n\t mz: " << rhs.mz \
-    << "\n\t altitude: " << rhs.altitude \
-    << "\n}" << std::endl;
+    out << "SENSORF: \n{\n\t ax: " 
+        << rhs.ax << "\n\t ay: " << rhs.ay << "\n\t az: " << rhs.az 
+        << "\n\t gx: " << rhs.gx << "\n\t gy: " << rhs.gy << "\n\t gz: " << rhs.gz 
+        << "\n\t mx: " << rhs.mx << "\n\t my: " << rhs.my << "\n\t mz: " << rhs.mz 
+        << "\n\t altitude: " << rhs.altitude 
+        << "\n}" << std::endl;
     return out;
 }
 
@@ -33,27 +33,21 @@ void sensor_to_float(const sensor &a, sensorf &f)
 
 void sensorf_to_planestate(const sensorf &data, PlaneState &p, float dt)
 {
-//    static float k_accel = 0.01, \
-//                 k_gyro = 0.0003, \
-//                 k_r = 0.01; // Kalman filter constants
+    //    static float k_accel = 0.01, 
+    //                 k_gyro = 0.0003, 
+    //                 k_r = 0.01; 
+    // Kalman filter constants
     // Create kalman filters to clean noisy sensor data
-//    static KalmanFilter filterXY(k_accel, k_gyro, k_r);
+    //    static KalmanFilter filterXY(k_accel, k_gyro, k_r);
     static Quaternion oldOrientation = Quaternion::identity;
-
+    
     Vector3d gravity_vector(data.ax, data.ay, data.az);
-    double ITheta = gravity_vector.magnitude() * dt;
-    Quaternion IGyroQuat = gravity_vector.rotationAroundAxis(ITheta);
+    Vector3d gyro_vector (data.gx, data.dy, data.gz);
+    double ITheta = gyro_vector.magnitude() * dt;
+    
+    Quaternion IGyroQuat = gyro_vector.rotationAroundAxis(ITheta);
     Quaternion AccOrientation = Vector3d::i.quaternionTo(gravity_vector);
     p.orientation = Quaternion::identity.slerp(oldOrientation * IGyroQuat, K_comp) *
-                    Quaternion::identity.slerp(AccOrientation, 1-K_comp);
-    // Create
-    
-
-    // Complimentary filter on orientation
-
-    
-    // Create a quaternion which rotates Vector3d::i to the gravity vector
-    //Quaternion attitude = Vector3d::i.quaternionTo(gvector);
-    //p.orientation = attitude;
+        Quaternion::identity.slerp(AccOrientation, 1-K_comp);
     oldOrientation = p.orientation;
 }
