fe877288c31b048641d569850da5e81dd30e4c5d
Jacob Sharf <jacobsharf@gmail.com>
2014-04-28 12:43:11 -0700

    Updated sensor filtering code

--- Comm/Sensor.cpp
+++ Comm/Sensor.cpp
@@ -44,11 +44,22 @@ void sensorf_to_planestate(const sensorf &data, PlaneState &p, float dt)
     Vector3d gravity_vector(data.ax, data.ay, data.az);
     Vector3d gyro_vector(data.gx, data.dy, data.gz);
     Vector3d mag_vector(data.mx, data.my, data.mz);
+
+    // orthonormal eigenbasis (u,v,w) using accelerometer and magnetic compass.
+    Vector3d u = gravity_vector.normalize();
+
+    Vector3d proj = mag_vector.project(u);
+    Vector3d perp = mag_vector - proj;
+    Vector3d v = perp.normalize();
+
+    Vector3d w = (u.cross(v)).normalize();
+
+    // perp and gy
+
     double ITheta = gyro_vector.magnitude() * dt;
     
     Quaternion IGyroQuat = gyro_vector.rotationAroundAxis(ITheta);
     Quaternion AccOrientation = Vector3d::i.quaternionTo(gravity_vector);
-    Quaternion MagOrientation = Vector3d::i.quaternionTo(mag_vector);
     p.orientation = Quaternion::identity.slerp(oldOrientation * IGyroQuat, K_comp) *
         Quaternion::identity.slerp(AccOrientation, 1-K_comp);
     oldOrientation = p.orientation;
