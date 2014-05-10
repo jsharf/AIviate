0923511771ff5142ac5ad63cd064b08028dbf625
Jacob Sharf <jacobsharf@gmail.com>
2014-05-09 17:10:40 -0700

    Updated compat_fg, now flightgear gives us angles in radians which are compatible with the fromEulerRad function from jsbsim's math library

--- fg_simu/compat_fg.cpp
+++ fg_simu/compat_fg.cpp
@@ -11,31 +11,21 @@ void usage()
 {
     std::cerr << "Usage: compat_fg <inport> <out addr> <out port>" << std::endl;
 }
-
 /// Return a quaternion from euler angles
-Quaternion fromEulerRad(fptype z, fptype y, fptype x)
+Quaternion fromEulerRad(fptype yaw, fptype pitch, fptype roll)
 {
-    fptype zd2 = fptype(0.5)*z; 
-    fptype yd2 = fptype(0.5)*y; 
-    fptype xd2 = fptype(0.5)*x;
+    Quaternion q;
+    fptype zd2 = fptype(0.5)*yaw; fptype yd2 = fptype(0.5)*pitch; fptype xd2 = fptype(0.5)*roll;
     fptype Szd2 = sin(zd2); fptype Syd2 = sin(yd2); fptype Sxd2 = sin(xd2);
     fptype Czd2 = cos(zd2); fptype Cyd2 = cos(yd2); fptype Cxd2 = cos(xd2);
     fptype Cxd2Czd2 = Cxd2*Czd2; fptype Cxd2Szd2 = Cxd2*Szd2;
     fptype Sxd2Szd2 = Sxd2*Szd2; fptype Sxd2Czd2 = Sxd2*Czd2;
-    
-    Quaternion q;
     q.w = Cxd2Czd2*Cyd2 + Sxd2Szd2*Syd2;
     q.x = Sxd2Czd2*Cyd2 - Cxd2Szd2*Syd2;
     q.y = Cxd2Czd2*Syd2 + Sxd2Szd2*Cyd2;
     q.z = Cxd2Szd2*Cyd2 - Sxd2Czd2*Syd2;
     return q;
 }
-
-Quaternion fromEulerDeg(fptype z, fptype y, fptype x)
-{
-    return fromEulerRad(DEG2RAD(z), DEG2RAD(y), DEG2RAD(x));
-}
-
 int main(int argc, char *argv[])
 {
     if (argc != 4)
@@ -50,25 +40,47 @@ int main(int argc, char *argv[])
 
     sensorf s;
     PlaneState p;
-
+/*
+    Vector3d rotate(const Vector3d& axis, fp_type theta) const;// Rotate the vector around the axis axis by the angle theta (+theta is CCW if vector points towards observer)
+*/
+    Vector3d axis;
+    fptype angle = 0;
     while (true)
     {
         lst.receiveSensor(s);
         
         // convert sensorf stuff to orientation in variable p
-        fptype roll, yaw, pitch;
-        roll = s.mx;
-        pitch = s.my;
-        yaw = s.mz;
-        
+        fptype phi, theta, psi;
+        phi = s.mx;
+        theta = s.my;
+        psi = s.mz;
+      
+        fptype roll, pitch, yaw;
+        roll = phi;
+        pitch = theta;
+        yaw = psi;
+       
         PlaneState p;
-        p.orientation = fromEulerDeg(yaw, roll, pitch);
 
-        Vector3d a = Vector3d::i.rotate(p.orientation);
-        cout << std::setw(8) <<  a.x << "\t";
-        cout << std::setw(8) << a.y << "\t";
-        cout << std::setw(8) <<  a.z << "\t" << endl;
+        Quaternion orientation = fromEulerRad(yaw, pitch, roll);
 
+        Vector3d axis = Vector3d::i.rotate(orientation);
+
+        p.orientation = orientation;
+/*
+        fptype lastangle = angle;
+        Vector3d lastaxis = axis;
+        axis = p.orientation.getAxisAngle(angle);
+*/
+        cout << "yaw: " << std::setw(8) <<  yaw << "\t";
+        cout << "pitch: " << std::setw(8) << pitch << "\t";
+        cout << "roll: " << std::setw(8) <<  roll << "\t";
+        cout << "i: " << std::setw(8) << axis.x << "\t";
+        cout << "j: " << std::setw(8) << axis.y << "\t";
+        cout << "k: " << std::setw(8) << axis.z << "\t";
+
+        //cout << "angle: " << std::setw(8) << angle << "\t"; 
+        cout << std::endl;
         snd.sendPlaneState(p);
     }
 }
